96e7318897ede11a5ca5732d12b20b7e721bb4bb
Jacob Gerlach <jwgerlach00@gmail.com>
2023-03-08 13:04:49 -0500

    added query (chatGPT lol)

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, request
 from flask_cors import CORS
 # import chemcuratepi
 from chemcurate import uniprot_mapping_filtered
+# import yaml
+
+# with open('test.yaml') as f:
+#     uniprot_mapping_filtered = yaml.load(f, Loader=yaml.FullLoader)
 
 
 app = Flask(__name__)
@@ -13,11 +17,26 @@ def get_organisms() -> list:
     print('ape')
     return list(uniprot_mapping_filtered.keys())
 
-@app.route('/get_proteins', methods=['POST'])
+@app.route('/proteins_for_organisms', methods=['POST'])
 def proteins_for_organisms() -> list:
     organisms = request.get_json()['organisms']
-    print(organisms)
-    return '', 204
+    # print(organisms)
+    # return '', 204
+    return list(uniprot_mapping_filtered[organisms[0]]['protein'].keys())
+
+def query_filter(items, value) -> list:
+    return list(filter(lambda e: (e or '').lower().find((value or '').lower()) > -1, items))
+
+@app.route('/query_organism_selections', methods=['POST'])
+def query_organism_selections() -> dict:
+    query = request.get_json()['query']
+    keys = query_filter(list(uniprot_mapping_filtered.keys()), query)
+    
+    out = {}
+    for key in keys:
+        out[key] = uniprot_mapping_filtered[key]
+        
+    return out
 
 # @app.route('/a', methods=['GET'])
 # def a() -> dict:
