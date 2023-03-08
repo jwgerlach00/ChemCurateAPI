a289e0e57f21531a50f5f2fb005d20765843de8c
Jacob Gerlach <jwgerlach00@gmail.com>
2023-03-07 21:45:13 -0500

    Need to move logic from client to API

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import flask
 from flask import Flask, request
 from flask_cors import CORS
-import chemcurate
+# import chemcuratepi
+from chemcurate import uniprot_mapping_filtered
 
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-@app.route('/get_uniprot_mapping', methods=['GET'])
-def get_uniprot_mapping() -> dict:
-    return chemcurate.uniprot_mapping
-    # return {'a': 'b'}
+@app.route('/get_organisms', methods=['GET'])
+def get_organisms() -> list:
+    print('ape')
+    return list(uniprot_mapping_filtered.keys())
 
-@app.route('/a', methods=['GET'])
-def a() -> dict:
-    return {'a': 'b',
-            'b': 'c'}
+@app.route('/get_proteins', methods=['POST'])
+def proteins_for_organisms() -> list:
+    organisms = request.get_json()['organisms']
+    print(organisms)
+    return '', 204
+
+# @app.route('/a', methods=['GET'])
+# def a() -> dict:
+#     return {'a': 'b',
+#             'b': 'c'}
 
 
 if __name__ == '__main__':
