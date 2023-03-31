9f863b3c93633cf9aa4501b2a80354b9212edf8a
jwgerlach00 <jwgerlach00@gmail.com>
2023-03-31 16:20:32 -0400

    changes for new mapper

--- main.py
+++ main.py
@@ -2,44 +2,61 @@ import flask
 from flask import Flask, Blueprint, request, session
 from flask_login import login_required, current_user
 from flask_cors import CORS
-from chemcurate import uniprot_mapping_filtered, Chembl, PubChem
+from chemcurate import uniprot_mapping, Chembl, PubChem
 
 
 main = Blueprint('main', __name__)
 
+display_name_map = {}
+organism_display_names = {}
+for db in uniprot_mapping.keys():
+    display_name_map[db] = {}
+    organism_display_names[db] = {}
+    for organism_sci_name in uniprot_mapping[db].keys():
+        organism_common_name = uniprot_mapping[db][organism_sci_name]['common_name']
+        organism_display_name = f'{organism_sci_name} ("{organism_common_name}")' if organism_common_name else organism_sci_name
+        organism_display_names[db][organism_display_name] = organism_sci_name
+        
+        protein_display_names = {}
+        for uniprot in uniprot_mapping[db][organism_sci_name]['protein'].keys():
+            protein_common_name = uniprot_mapping[db][organism_sci_name]['protein'][uniprot]['common_name']
+            protein_display_names[f'{uniprot} ("{protein_common_name}")' if protein_common_name else uniprot] = uniprot
+            
+        display_name_map[db][organism_display_name] = protein_display_names
 
-display_name_mapper = {}
-organism_display_name_mapper = {}
-protein_display_name_mapper = {}
-for key in list(uniprot_mapping_filtered.keys()):
-    common_name = uniprot_mapping_filtered[key]['common_name']
-    
-    organism_display_name = f'{key} ("{common_name}")' if common_name else key
-    organism_display_name_mapper[organism_display_name] = key
-    
-    protein_names = list(uniprot_mapping_filtered[key]['protein'].keys())
-    
-    protein_display_names = []
-    for protein in protein_names:
-        protein_display_names.append('{0} ({1})'.format(protein, ', '.join(uniprot_mapping_filtered[key]['protein'][protein])))
-        protein_display_name_mapper[protein_display_names[-1]] = protein
-    
-    display_name_mapper[organism_display_name] = protein_display_names
-    
 
-def undo_display_names(organism_protein_map:dict) -> dict:
+def undo_display_protein(organism_protein_map:dict) -> dict:
+    '''
+    DISPLAY_NAME_MAP
+    ----------------
+    DB: {
+        ORGANISM_DISPLAY_NAME: {
+            PROTEIN_DISPLAY_NAME: UNIPROT_ID
+        }
+    }
+    
+    ORGANISM_DISPLAY_NAMES
+    ----------------------
+    DB: {
+        ORGANISM_DISPLAY_NAME: ORGANISM_SCI_NAME
+    }
+    '''
+    out = {}
+    for db in organism_protein_map.keys():
+        out[db] = {}
+        out[db]
     out = {}
     for (organism, proteins) in organism_protein_map.items():
-        organism_name = organism_display_name_mapper[organism]
-        protein_names = [protein_display_name_mapper[p] for p in proteins]
+        organism_name = organism_display_names[organism]
+        protein_names = [protein_display_names[p] for p in proteins]
         out[organism] = []
         for p in protein_names:
-            out[organism] += uniprot_mapping_filtered[organism_name]['protein'][p]
+            out[organism] += uniprot_mapping[organism_name]['protein'][p]
     return out
 
 @main.route('/get_uniprot_map', methods=['GET'])
 def get_uniprot_map() -> dict:
-    return display_name_mapper
+    return display_name_map
 
 @main.route('/submit', methods=['POST'])
 @login_required
